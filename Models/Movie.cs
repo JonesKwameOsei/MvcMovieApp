e6233cd4f0d709c9ead80872425d0e0229ff9840
JonesKwameOsei <oseikwamejones@gmail.com>
2025-10-26 22:17:35 +0000

    Add ReleaseDate, Price, and database seeding logic
    Enhanced the `Movie` model by adding `ReleaseDate` and `Price` properties with data annotations for validation and database schema configuration.
    
    Introduced a new `SeedData` class to populate the database with initial movie data if empty. Updated `Program.cs` to integrate the seeding process during application startup.
    
    Added necessary `using` directives to support the new functionality.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models;
 
@@ -6,8 +8,12 @@ public class Movie
 {
     public int Id { get; set; }
     public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
+    [Column(TypeName = "decimal(5, 2)")]
+    [Range(0.01, 999.99, ErrorMessage = "Price must be between £0.01 to £999.99")]
     public decimal Price { get; set; }
 }
