0d8b2be49e8ef74b6b50663bfaa606b2ed462306
JonesKwameOsei <oseikwamejones@gmail.com>
2025-10-27 03:36:30 +0000

    Enhance validation and UI for Movie model and forms
    Updated the `Movie` model to include stricter validation rules:
    - Added validation attributes for `Title`, `Genre`, `Rating`, and `Price`.
    - Adjusted `Price` range and added currency formatting.
    
    Reorganized `Rating` input field in `Create.cshtml` and `Edit.cshtml`:
    - Moved `Rating` field to follow the `Price` field for better form structure.
    
    Improved button styling in `site.css`:
    - Added margin to `.btn-primary` for consistent spacing.

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -6,14 +6,19 @@ namespace MvcMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3), Required]
     public string? Title { get; set; }
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
-    [Column(TypeName = "decimal(5, 2)")]
-    [Range(0.01, 999.99, ErrorMessage = "Price must be between £0.01 to £999.99")]
+
+    [Range(1, 100, ErrorMessage = "Price must be between £1 to £100"), Column(TypeName = "decimal(5, 2)"), DataType(DataType.Currency)]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+    public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
     public string? Rating { get; set; }
 }
